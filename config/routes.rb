f0bc20084a77ccaaddb5f47204c757963dbf7d00
Manu Cheremeh <manucheremeh1995@gmail.com>
2020-10-01 00:01:31 +0000

    creating user

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  resources :users
+  get 'users/new'
+  get 'users/create'
+  get 'home/index'
+  root 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

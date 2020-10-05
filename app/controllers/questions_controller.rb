599f864e5e18d0263c60a28d4b52a12fe0dd3bab
Manu Cheremeh <manucheremeh1995@gmail.com>
2020-10-05 22:58:43 +0000

    Question model and controller

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,14 @@
+class QuestionsController < ApplicationController
+  def index
+    @questions  = Question.order(created_at: :desc).all
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def delete
+  end
+end

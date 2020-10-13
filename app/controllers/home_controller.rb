a707d44df20648c716c11ce33f1dda453316c808
Manu Cheremeh <manucheremeh1995@gmail.com>
2020-10-13 23:58:08 +0000

    changes to home_controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,16 @@
 class HomeController < ApplicationController
+  def index
+    # render('index')
+    redirect_to controller: 'home', action: 'index'
+  end
+
+  def show
+    # render('home/show')
+    redirect_to(controller: 'home', action: 'index')
+  end
+end
+
+def login
+  @plural = [2, 3, 4, 5, 6, 7, 8, 9]
+  # render 'new'
 end

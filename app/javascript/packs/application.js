9114087e5a2b94f68ee179ad11ac350f9fdc0ec5
Manu Cheremeh <manucheremeh1995@gmail.com>
2020-12-23 23:59:35 +0000

    changes:Modification to stylesheet & bootstrap inclusion

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,6 +10,7 @@
 // const imagePath = (name) => images(name, true)
 
 import "bootstrap";
+import "@fortawesome/fontawesome-free/js/all";
 require("@rails/ujs").start();
 require("turbolinks").start();
 require("@rails/activestorage").start();
